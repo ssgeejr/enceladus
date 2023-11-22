989330f8f2eb601c9f017d9a631df5ff99967182
ioexcept <ioexcept@yahoo.com>
2023-11-22 14:34:54 -0600

    value objects now working! next step is to add the MYSQL code to save all this data.

--- mysql/init.sql
+++ mysql/init.sql
@@ -3,7 +3,7 @@ SET GLOBAL time_zone = 'America/Chicago';
 USE enceladus;
 
 CREATE TABLE denials (
-	denialid int NOT NULL AUTO_INCREMENT,,
+	denialid int NOT NULL AUTO_INCREMENT,
     groupname varchar(64) NOT NULL,
 	filedate Date NOT NULL,
 	grandtotal float(8,2) NOT NULL default 0.0,
@@ -12,7 +12,7 @@ CREATE TABLE denials (
 );
 
 CREATE TABLE adjustments (
-	adjustmentid int NOT NULL AUTO_INCREMENT,,
+	adjustmentid int NOT NULL AUTO_INCREMENT,
     claimid varchar(16) NOT NULL,
 	filedate Date NOT NULL,
 	grandtotal float(8,2) NOT NULL default 0.0,
